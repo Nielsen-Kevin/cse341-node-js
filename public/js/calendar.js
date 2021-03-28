3cb6712a03014cfd334144a14680f2483f180eb3
kevinsnielsen <forgeofatlas@gmail.com>
2021-03-28 02:55:03 -0700

    w12

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -436,10 +436,15 @@ function startCalendar() {
 				let events = JSON.parse(this.responseText);
 				console.log(events);
 
-				for(let key in events) {
-					id = NewEvent(events[key].name, numMonth, parseInt(events[key].day, 10), numYear, events[key].time, events[key].color);
-					mSchedule[id].updateDay();
-					mSchedule[id].recordId = events[key].id;
+				for(let k in events) {
+					let key = 'e' + eventCount;
+					let record = new DateObject(key, 'event', events[k].name, parseInt(events[k].day, 10), numMonth, numYear, events[k].time, events[k].color);
+					if(numYear == year && numMonth == month) {
+						mSchedule[key] = record;
+						mSchedule[key].updateDay();
+						mSchedule[key].recordId = events[k].id;
+					}
+					autoIncrement();
 				}
 			}
 			if (this.status == 500) {
